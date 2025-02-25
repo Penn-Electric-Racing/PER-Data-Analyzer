4ca0ebcd0bd50649386ff470795ed56c228cf63e
laytcai <1715782167@qq.com>
2025-02-24 20:31:47 -0500

    Changed names

--- Tests/test.py
+++ Tests/test.py
@@ -1,7 +1,9 @@
-import PERalyzer  # or from PERalyzer import Analyzer
+import peralyzer  # or from PERalyzer import Analyzer
+from peralyzer import Plotter
 
 def main():
-    analyzer = PERalyzer.Analyzer()
+    analyzer = peralyzer.Analyzer()
+    analyzer.analyze()
     # or analyzer = Analyzer()
     # do something with `analyzer`
 
