59d53e6819bac5f7d8f8dee2874f9c8fdd1392c2
laytcai <1715782167@qq.com>
2025-02-26 15:16:36 -0500

    Add time slicing feature

--- perda/__init__.py
+++ perda/__init__.py
@@ -1,21 +1,7 @@
 # peralyzer/__init__.py
 from .analyzer import analyzer  # Import Analyzer from core.py
 
-__all__ = ["analyzer", "init", "readCsv", "plot"]  # Defines what gets imported with `from peralyzer import *`
+__all__ = ["create"]  # Defines what gets imported with `from peralyzer import *`
 
-# Create a global instance of `analyzer`
-analyzer_instance = None
-
-def init():
-    global analyzer_instance 
-    analyzer_instance = analyzer()
-
-def read_csv(csvPath):
-    if analyzer_instance is None:
-        raise RuntimeError("Call init() before using readCsv()")
-    analyzer_instance.read_csv(csvPath)
-
-def plot(variables, same_graph, unit_ms):
-    if analyzer_instance is None:
-        raise RuntimeError("Call init() before using plot()")
-    analyzer_instance.plot(variables, same_graph, 0, -1, unit_ms)
+def create():
+    return analyzer()
--- perda/analyzer.py
+++ perda/analyzer.py
@@ -4,28 +4,52 @@ import matplotlib.pyplot as plt
 
 class analyzer:
     def __init__(self):
-        self.value_map = {}
-        self.ID_map = {}
-        self.high_voltage_changes = []
-        self.file_read = False
-        self.start_time = None
-        self.end_time = None
-        print("Analyzer Initialized")
+        self.__value_map = {}
+        self.__ID_map = {}
+        self.__high_voltage_changes = []
+        self.__file_read = False
+        self.__data_start_time = None
+        self.__data_end_time = None
+
+        self.__plot_same_graph = True
+        self.__plot_start_time = 0
+        self.__plot_end_time = -1
+        self.__plot_unit = "s"
+
+        print("Analyzer Created")
 
     def reset(self):
-        self.value_map = {}
-        self.ID_map = {}
-        self.high_voltage_changes = []
-        self.file_read = False
-        self.start_time = None
-        self.end_time = None
+        self.__value_map = {}
+        self.__ID_map = {}
+        self.__high_voltage_changes = []
+        self.__file_read = False
+        self.__data_start_time = None
+        self.__data_end_time = None
+
+        self.__plot_same_graph = True
+        self.__plot_start_time = 0
+        self.__plot_end_time = -1
+        self.__plot_unit = "s"
+
         print("Reset Analyzer")
 
     def read_csv(self, path):
-        if self.file_read:
+        if self.__file_read:
             print("Call .reset() before reading new csv")
             return
-        
+        # Reset but do not print
+        self.__value_map = {}
+        self.__ID_map = {}
+        self.__high_voltage_changes = []
+        self.__file_read = False
+        self.__data_start_time = None
+        self.__data_end_time = None
+
+        self.__plot_same_graph = True
+        self.__plot_start_time = 0
+        self.__plot_end_time = -1
+        self.__plot_unit = "s"
+
         time_stamp = 0
         start_time_read = False
         high_voltage = False
@@ -40,7 +64,7 @@ class analyzer:
                     if (line.startswith("Value")):
                         canID_name_value = line[6:].strip().split(": ")
                         try:
-                            self.ID_map[int(canID_name_value[1])] = canID_name_value[0]
+                            self.__ID_map[int(canID_name_value[1])] = canID_name_value[0]
                         except Exception as e:
                             print(f"Error parsing ID | Line number {line_num} | Line: {line} | Error: {e}")
                             break
@@ -48,40 +72,50 @@ class analyzer:
                         data = line.strip().split(",")
                         try:
                             id = int(data[1])
-                            name = self.ID_map[id]
+                            name = self.__ID_map[id]
                             val = float(data[2])
                             raw_time = int(data[0])
 
-                            if not start_time_read and self.name_matches("sdl.startTime", name):
-                                self.start_time = val
+                            if not start_time_read and self.__name_matches("sdl.startTime", name):
+                                self.__data_start_time = val
                                 start_time_read = True
-                            elif self.name_matches("sdl.currentTime", name):
+                            elif self.__name_matches("sdl.currentTime", name):
                                 time_stamp = val / 1e3
-                            elif self.name_matches("ams.airsState", name):
+                            elif self.__name_matches("ams.airsState", name):
                                 if high_voltage and val == 0:
                                     high_voltage = False
-                                    self.high_voltage_changes.append((time_stamp, high_voltage))
+                                    self.__high_voltage_changes.append((time_stamp, high_voltage))
                                 elif not high_voltage and val == 4:
                                     high_voltage = True
-                                    self.high_voltage_changes.append((time_stamp, high_voltage))
-                            elif self.name_matches("pcm.pedals.implausibility.anyImplausibility", name):
+                                    self.__high_voltage_changes.append((time_stamp, high_voltage))
+                            elif self.__name_matches("pcm.pedals.implausibility.anyImplausibility", name):
                                 any_implausibility = val
 
-                            if name not in self.value_map:
-                                self.value_map[name] = []
+                            if name not in self.__value_map:
+                                self.__value_map[name] = []
                             
-                            self.value_map[name].append([time_stamp, val, high_voltage, any_implausibility, raw_time])
+                            self.__value_map[name].append([time_stamp, val, high_voltage, any_implausibility, raw_time])
 
                         except Exception as e:
                             print(f"Error parsing ID | Line number {line_num} | Line: {line} | Error: {e}")
                             break
                     line_num += 1
                     pbar.update(1)
-        self.end_time = raw_time
-        self.file_read = True
+        self.__data_end_time = raw_time
+        self.__file_read = True
 
-    def plot(self, variables, start_time = 0, end_time = -1, same_graph = True, unit_ms = False):
-        if not self.file_read:
+    def set_plot(self, start_time = 0, end_time = -1, same_graph = True, unit = "s"):
+        self.__plot_start_time = start_time
+        self.__plot_end_time = end_time
+        self.__plot_same_graph = same_graph
+        if unit != "s" and unit != "ms" and unit != "us":
+            print("Invalid unit input, please use \"s\", \"ms\", or \"us\". Default to s.")
+            self.__plot_unit = "s"
+        else:
+            self.__plot_unit = unit
+
+    def plot(self, variables):
+        if not self.__file_read:
             print("No csv read. Call .read_csv() before plotting.")
             return
         for var in variables:
@@ -90,42 +124,64 @@ class analyzer:
                 continue
 
             short_name = var
-
-            vals = self.find_variable(short_name, self.value_map)
-
+            vals = self.get_np_array(short_name)
             if vals is None:
                 continue
 
-            if not unit_ms:
-                t = vals[::, 0]/1e3
+            last_time_stamp = self.__plot_end_time
+            first_time_stamp = self.__plot_start_time
+
+            if self.__plot_unit == "s":
+                last_time_stamp *= 1e3
+                first_time_stamp *= 1e3
+            elif self.__plot_unit == "us":
+                last_time_stamp /= 1e3
+                first_time_stamp /= 1e3
+            
+            if self.__plot_end_time == -1:
+                last_time_stamp = vals[-1,0]
+            
+            filtered_vals = vals[(vals[:, 0] >= first_time_stamp) & (vals[:, 0] <= last_time_stamp )]
+
+            if self.__plot_unit == "s":
+                t = filtered_vals[::, 0]/1e3
                 plt.xlabel("Timestamp (s)")
-            else:
-                t = vals[::, 0]
+            elif self.__plot_unit == "ms":
+                t = filtered_vals[::, 0]
                 plt.xlabel("Timestamp (ms)")
+            else:
+                t = filtered_vals[::, 0]*1e3
+                plt.xlabel("Timestamp (us)")
 
-            y = vals[::, 1]
+            for hvT, hvOn in self.__high_voltage_changes:
+                color = "red" if hvOn else "green"
+                if (hvT >= first_time_stamp and hvT <= last_time_stamp):
+                    if self.__plot_unit == "s":
+                        hvT = hvT/1e3
+                    elif self.__plot_unit == "us":
+                        hvT = hvT* 1e3
+                    plt.axvline(x=hvT, color=color)
 
+            y = filtered_vals[::, 1]
             plt.plot(t, y, label=short_name)
             plt.legend()
 
-            for hvT, hvOn in self.high_voltage_changes:
-                hvT = hvT if unit_ms else hvT/1e3
-                color = "red" if hvOn else "green"
-                plt.axvline(x=hvT, color=color)
-
-            if not same_graph:
+            if not self.__plot_same_graph:
                 plt.suptitle(short_name)
                 plt.show()
                 print("\n\n")
-
-        if same_graph:
+        
+        if self.__plot_same_graph:
             plt.show()
             print("\n\n")
 
-    def find_variable(self, short_name, value_map):
+    def get_np_array(self, short_name):
+        if not self.__file_read:
+            print("No csv read. Call .read_csv() before finding variable.")
+            return None
         full_name = None
-        for var_name in value_map.keys():
-            if self.name_matches(short_name, var_name):
+        for var_name in self.__value_map.keys():
+            if self.__name_matches(short_name, var_name):
                 full_name = var_name
                 break
 
@@ -133,7 +189,7 @@ class analyzer:
             print("Error: could not find data for " + short_name)
             return None
 
-        return np.array(value_map[full_name])
+        return np.array(self.__value_map[full_name])
     
-    def name_matches(self, short_name, full_name):
+    def __name_matches(self, short_name, full_name):
         return f'({short_name})' in full_name
