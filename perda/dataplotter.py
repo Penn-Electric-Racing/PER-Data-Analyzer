399c73c5442f29c5db9f1bed1edd336b0c5a384f
laytcai <1715782167@qq.com>
2025-06-20 16:06:12 -0400

    Added join graph

--- perda/dataplotter.py
+++ perda/dataplotter.py
@@ -211,7 +211,86 @@ class dataplotter:
                     print(f"graph for {title}")
                     plt.show()
                     print("\n\n")
+       
+    def plot_dual_group(self, l_v: list, r_v: list, left_title: str = "Left", right_title: str = "Right", middle_title: str = "title", labels = False, h_lines: list = [], v_lines: list = []):
+        if not self.__file_read:
+            raise AttributeError("No csv read. Call .get_csvparser() before plotting.")
+        left_len = len(l_v)
+        both_l = l_v + r_v
+        xlabel = "Timestamp (s)" if self.__plot_unit == "s" else "Timestamp (ms)"
+        fig, ax1 = plt.subplots()
+        ax2 = None
+        colors = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+        color_index = 0
 
+
+        for h in h_lines:
+            plt.axhline(y=h, color='black', linestyle='--', zorder=1)
+        
+        for v in v_lines:
+            plt.axvline(x=v, color='black', linestyle='--', zorder=1)
+
+        for index, var in enumerate(both_l):
+            if type(var) is str:
+                short_name = var
+                vals = self.__csvparser.get_np_array(short_name)
+            elif isinstance(var, np.ndarray):
+                if var.ndim != 2 or var.shape[1] < 2:
+                    print("Invalid Input Array For Plotting")
+                    continue
+                short_name = "Input Array"
+                vals = var
+            else:
+                print("Invalid Input For Plotting")
+                continue
+
+            last_time_stamp = self.__plot_end_time
+            first_time_stamp = self.__plot_start_time
+
+            if self.__plot_unit == "s":
+                last_time_stamp *= 1e3
+                first_time_stamp *= 1e3
+            
+            if self.__plot_end_time == -1:
+                last_time_stamp = vals[-1,0]
+            
+            filtered_vals = vals[(vals[:, 0] >= first_time_stamp) & (vals[:, 0] <= last_time_stamp )]
+            if filtered_vals.size == 0:
+                print(f"Warning: No data available for {short_name} in the given time range.")
+                continue
+
+            if self.__plot_unit == "s":
+                t = filtered_vals[::, 0]/1e3
+            else:
+                t = filtered_vals[::, 0]
+
+            for hvT, hvOn in self.__csvparser.get_HV_changes():
+                color = "red" if hvOn else "green"
+                if (hvT >= first_time_stamp and hvT <= last_time_stamp):
+                    if self.__plot_unit == "s":
+                        hvT = hvT/1e3
+                    plt.axvline(x=hvT, color=color)
+
+            y = filtered_vals[::, 1]
+
+            if index < left_len:
+                ax1.plot(t, y, label=short_name, color = colors[index % len(colors)])
+                ax1.set_ylabel(left_title)
+                ax1.tick_params(axis='y')
+            else:
+                if ax2 is None:
+                    ax2 = ax1.twinx()
+                ax2.plot(t, y, label=short_name, color = colors[index % len(colors)]) #, linestyle="--"
+                ax2.set_ylabel(right_title)
+                ax2.tick_params(axis='y')
+        plt.xlabel(xlabel)
+        plt.title(f"Join Graph for {middle_title}")
+        if labels:
+            ax1.legend(loc='upper left')
+            ax2.legend(loc='upper right')
+        fig.tight_layout()
+        plt.show()
+    
     def plot0to60(self, numWheels):
         self.set_plot()
         short_names = ["pcm.wheelSpeeds.frontLeft", "pcm.wheelSpeeds.frontRight", 
