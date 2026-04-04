9330e69710695103bc5bb8ed2ad1dd6c89c9a7a4
laytcai <1715782167@qq.com>
2026-04-04 14:04:56 -0400

    :(((

--- perda/__init__.py
+++ perda/__init__.py
@@ -1,13 +1,13 @@
-# from pathlib import Path
+from pathlib import Path
 
-# from sentence_transformers.cross_encoder import CrossEncoder
+from sentence_transformers.cross_encoder import CrossEncoder
 
-# _MODEL_DIR = Path(__file__).parent / "models" / "stsb-cross-encoder"
-# _HF_MODEL_ID = "cross-encoder/stsb-distilroberta-base"
+_MODEL_DIR = Path(__file__).parent / "models" / "stsb-cross-encoder"
+_HF_MODEL_ID = "cross-encoder/stsb-distilroberta-base"
 
-# if not _MODEL_DIR.exists():
-#     print("Downloading cross-encoder model (one-time setup)...")
-#     _model = CrossEncoder(_HF_MODEL_ID)
-#     _MODEL_DIR.parent.mkdir(parents=True, exist_ok=True)
-#     _model.save(str(_MODEL_DIR))
-#     print(f"Model saved to: {_MODEL_DIR}")
+if not _MODEL_DIR.exists():
+    print("Downloading cross-encoder model (one-time setup)...")
+    _model = CrossEncoder(_HF_MODEL_ID)
+    _MODEL_DIR.parent.mkdir(parents=True, exist_ok=True)
+    _model.save(str(_MODEL_DIR))
+    print(f"Model saved to: {_MODEL_DIR}")
